acd2ba6858a1600eadd0bed6e6bac6195a59ea02
Kev-BB <118001660+Kev-BB@users.noreply.github.com>
2024-02-24 13:18:37 +0300

    Updated Content

---
+++ src/components/Header.tsx
@@ -0,0 +1,49 @@
+import {
+  Image,
+  Container,
+  Title,
+  Button,
+  Group,
+  Text,
+  List,
+  ThemeIcon,
+  rem,
+} from "@mantine/core";
+import classes from "./Header.module.css";
+
+export function Header() {
+  return (
+    <Container size="md">
+      <div className={classes.inner}>
+        <div className={classes.content}>
+          <Title className={classes.title}>
+            A <span className={classes.highlight}>modern</span> React <br />{" "}
+            components library
+          </Title>
+          <Text  mt="md">
+            Build fully functional accessible web applications faster than ever
+            – Mantine includes more than 120 customizable components and hooks
+            to cover you in any situation
+          </Text>
+          <Group mt={30}>
+            <Button radius="xl" size="md" color="dark" className={classes.control}>
+              Get started
+            </Button>
+            <Button
+              variant="default"
+              radius="xl"
+              size="md"
+              className={classes.control}
+            >
+              Source code
+            </Button>
+          </Group>
+        </div>
+        <Image
+          src="https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?q=80&w=1945&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className={classes.image}
+        />
+      </div>
+    </Container>
+  );
+}
